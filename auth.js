12a797f2148f2bc688ad4c9a998721660b34c384
aniket.surve <aniket.surve@synechron.com>
2026-02-17 19:35:50 -0500

    Added auth for priviledged pages

--- auth.js
+++ auth.js
@@ -9,17 +9,22 @@ function logout() {
 function checkAuth() {
     const token = localStorage.getItem("id_token");
     if (!token) {
-        const cognitoDomain = "https://us-east-1senj5cvnr.auth.us-east-1.amazoncognito.com";
-        const clientId = "1u4dbf3uu4s4fvnb4g7lo9o59";
-        const redirectUri = "https://www.agastyagpt.com/callback.html";
+        try {
+            const cognitoDomain = "https://us-east-1senj5cvnr.auth.us-east-1.amazoncognito.com/login";
+            const clientId = "1u4dbf3uu4s4fvnb4g7lo9o59";
+            const redirectUri = "https://www.agastyagpt.com/callback.html";
 
-        document.getElementById("signuploginBtn").href =
-            `${cognitoDomain}/login` +
-            `?client_id=${clientId}` +
-            `&redirect_uri=${encodeURIComponent(redirectUri)}` +
-            `&response_type=token` +
-            `&scope=email+openid+phone`;
-        // window.location.href = '/';
+            window.location.href =
+                `${cognitoDomain}` +
+                `?client_id=${clientId}` +
+                `&redirect_uri=${encodeURIComponent(redirectUri)}` +
+                `&response_type=token` +
+                `&scope=email+openid+phone`;
+        }
+        catch (error) {
+            console.error("Error redirecting to login:", error);
+            window.location.href = '/';
+        }
     }
     return true;
 }
